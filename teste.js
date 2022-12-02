6a36d584c63ce697bc5b0bb13b6d73cc5340e5cc
Arthur <arthurfelixcruz@gmail.com>
2022-12-01 23:08:07 -0300

    classe corrida

--- teste.js
+++ teste.js
@@ -1,39 +1,68 @@
 class Carro{
     
-    Nome
-    Potencia
-    VelocidadeMaxima
-    Aceleracao
+    Nome = []
+    Potencia = []
+    VelocidadeMaxima = []
+    Aceleracao = []
     
     velocidadeMedia(distancia){
-        tempo = distancia / (this.VelocidadeMaxima/this.Aceleracao)
+        let tempoEmSegundos = []
+        let tempo = 0
+        for (let index = 0; index < 4; index++) {
+            tempo = distancia / (this.VelocidadeMaxima[index]/this.Aceleracao[index])
+            tempoEmSegundos.push(tempo)
+       } 
+        return tempoEmSegundos
     }
 }
 
-distancia = parseInt(prompt('digite uma distancia (em metros) para percorrer'))
+let carros = new Carro() 
 
-let ferrari = new Carro() 
-ferrari.Nome = 'Ferrari F8 Tributo'
-ferrari.Potencia = 720
-ferrari.VelocidadeMaxima = 340
-ferrari.Aceleracao = 34
+carros.Nome[0] = 'Ferrari F8 Tributo'
+carros.Potencia[0] = 720
+carros.VelocidadeMaxima[0] = 340
+carros.Aceleracao[0] = 34
 
-let porsche = new Carro() 
-porsche.Nome = 'Porsche Panamera'
-porsche.Potencia = 700
-porsche.VelocidadeMaxima = 315
-porsche.Aceleracao = 31
+carros.Nome[1] = 'Porsche Panamera'
+carros.Potencia[1] = 700
+carros.VelocidadeMaxima[1] = 315
+carros.Aceleracao[1] = 31
 
-let audi = new Carro() 
-audi.Nome = 'Audi R8'
-audi.Potencia = 610
-audi.VelocidadeMaxima = 320
-audi.Aceleracao = 32
+carros.Nome[2] = 'Audi R8'
+carros.Potencia[2] = 610
+carros.VelocidadeMaxima[2] = 320
+carros.Aceleracao[2] = 32
+ 
+carros.Nome[3] = 'Mustang Shelby GT500'
+carros.Potencia[3] = 771
+carros.VelocidadeMaxima[3] = 320
+carros.Aceleracao[3] = 30
 
-let mustang = new Carro() 
-mustang.Nome = 'Mustang Shelby GT500'
-mustang.Potencia = 771
-mustang.VelocidadeMaxima = 320
-mustang.Aceleracao = 30
+class Corrida{
+    Nome
+    Tipo
+    Distancia
+    Vencedor
 
-let carros = [ferrari, porsche, audi, mustang]
+    verificarVencedor(carros){
+        let menorTempo = 1000000000000
+        let tempo = carros.velocidadeMedia(this.Distancia)
+        
+        for (let index = 0; index < 4; index++) {
+            if (menorTempo > tempo[index]) {
+                menorTempo = tempo[index]
+                this.Vencedor = carros.Nome[index]
+            }
+        }
+        return this.Vencedor
+    }
+
+    mostrarVencedor(){
+        console.log('o vencedor foi: ' + this.Vencedor)
+    }
+}
+
+let F1 = new Corrida()
+F1.Nome = "Racha na BR"
+F1.Tipo = "Corrida de rua"
+F1.Distancia = 1500
